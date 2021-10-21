d9879acd650d984e8c7c642a39e1fd58567f3311
Artem Chebotarev <artemchebotarev@Artems-MacBook-Pro.local>
2021-10-21 18:58:57 +0300

    initial

---
+++ config/webpack.dev.config.ts
@@ -0,0 +1,62 @@
+import ESLintPlugin from 'eslint-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import {Configuration} from 'webpack';
+
+const config: Configuration = {
+    mode: 'development',
+    output: {
+        publicPath: '/',
+    },
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+                    },
+                },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+        new ForkTsCheckerWebpackPlugin({
+            async: false,
+        }),
+        new ESLintPlugin({
+            extensions: ['js', 'jsx', 'ts', 'tsx'],
+        }),
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        static: path.join(__dirname, 'build'),
+        historyApiFallback: true,
+        port: 4000,
+        open: true,
+        hot: true,
+    },
+};
+
+export default config;
